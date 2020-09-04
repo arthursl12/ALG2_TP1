6f6e0aa214ec3a914320c29fdb4a641305056702
arthursl12 <arthursoutolima@gmail.com>
2020-09-04 07:17:36 -0300

    Refatoração testes main

--- test_main.py
+++ test_main.py
@@ -3,18 +3,25 @@ from main import arquivoString
 
 NOME_ARQ = 'sarscov2.fasta'
 
+# O texto a ser processado, i.e. o genoma do Sars-Cov-2 (Corona vírus), deverá
+# ser carregado para memória principal em estrutura de dados compacta, representando
+# um vetor de caracteres
+
+
+
 class Test_arquivoString:
-    def test_sem_excecao(self):
-        arquivoString(NOME_ARQ)
+    @classmethod
+    def setup_class(cls):
+        cls.string = arquivoString(NOME_ARQ)
+
     def test_tipo_retorno(self):
-        string = arquivoString(NOME_ARQ)
-        assert type(string) == str
+        assert type(self.string) == str
     def test_sem_espacos(self):
-        string = arquivoString(NOME_ARQ)
+        string = self.string
         for i in range(len(string)):
             assert string[i].isspace() == False, f"erro na posição {i}"
     def test_sem_cabecalho(self):
-        string = arquivoString(NOME_ARQ)
+        string = self.string
         for i in range(len(string)):
             assert string[i].isupper() == True, f"erro na posição {i}"
 
